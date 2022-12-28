f972c19d6c27783232d5ca3bbf0a9fa1c5cad8c0
oysteinbakken <oystein.bakken@nav.no>
2022-12-28 17:34:14 +0100

    Stage 6/6: Brush up your code

--- Coffee Machine/task/src/machine/CoffeeMachine.kt
+++ Coffee Machine/task/src/machine/CoffeeMachine.kt
@@ -1,31 +1,51 @@
 package machine
 
-class CoffeeMachine(
-    private var water: Int = 400,
-    private var milk: Int = 540,
-    private var beans: Int = 120,
-    private var cups: Int = 9,
-    private var money: Int = 550
-) {
-    fun run() {
-        do {
-            println("Write action (buy, fill, take, remaining, exit):")
-            when (readln()) {
-                "buy" -> buyCoffee()
-                "fill" -> fillIngredients()
-                "take" -> takeMoney()
-                "remaining" -> printStatus()
-                "exit" -> return
-            }
-        } while (true)
-    }
+enum class State { IDLE, BUY, FILL_WATER, FILL_MILK, FILL_BEANS, FILL_CUPS }
 
-    private fun buyCoffee() {
-        when (requestInput("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")) {
-            "1" -> makeCoffee(250, 0, 16, 4)
-            "2" -> makeCoffee(350, 75, 20, 7)
-            "3" -> makeCoffee(200, 100, 12, 6)
-            "back" -> return
+class CoffeeMachine {
+    private var water: Int = 400
+    private var milk: Int = 540
+    private var beans: Int = 120
+    private var cups: Int = 9
+    private var money: Int = 550
+    private var state = State.IDLE
+
+    fun processInput(userInput: String) {
+        when (state) {
+            State.IDLE ->
+                when (userInput) {
+                    "buy" -> state = State.BUY
+                    "fill" -> state = State.FILL_WATER
+                    "take" -> takeMoney()
+                    "remaining" -> printStatus()
+                    "exit" -> return
+                }
+
+            State.BUY -> {
+                when (userInput) {
+                    "1" -> makeCoffee(250, 0, 16, 4)
+                    "2" -> makeCoffee(350, 75, 20, 7)
+                    "3" -> makeCoffee(200, 100, 12, 6)
+                    "back" -> state = State.IDLE
+                }
+            }
+
+            State.FILL_WATER -> {
+                this.water += userInput.toInt()
+                state = State.FILL_MILK
+            }
+            State.FILL_MILK -> {
+                this.milk += userInput.toInt()
+                state = State.FILL_BEANS
+            }
+            State.FILL_BEANS -> {
+                this.beans += userInput.toInt()
+                state = State.FILL_CUPS
+            }
+            State.FILL_CUPS -> {
+                this.cups += userInput.toInt()
+                state = State.IDLE
+            }
         }
     }
 
@@ -52,18 +72,13 @@ class CoffeeMachine(
         this.milk -= milk
         this.beans -= beans
         this.money += money
-    }
-
-    private fun fillIngredients() {
-        water += requestInput("Write how many ml of water you want to add:").toInt()
-        milk += requestInput("Write how many ml of milk you want to add:").toInt()
-        beans += requestInput("Write how many grams of coffee beans you want to add:").toInt()
-        cups += requestInput("Write how many disposable cups you want to add:").toInt()
+        state = State.IDLE
     }
 
     private fun takeMoney() {
         println("I gave you \$$money")
         money = 0
+        state = State.IDLE
     }
 
     private fun printStatus() {
@@ -75,8 +90,14 @@ class CoffeeMachine(
         println("$$money of money")
     }
 
-    private fun requestInput(prompt: String): String {
-        println(prompt)
-        return readln().trim().lowercase()
+    override fun toString(): String {
+        return when (state) {
+            State.IDLE -> "Write action (buy, fill, take, remaining, exit):"
+            State.BUY -> "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:"
+            State.FILL_WATER -> "Write how many ml of water you want to add:"
+            State.FILL_MILK -> "Write how many ml of milk you want to add:"
+            State.FILL_BEANS -> "Write how many grams of coffee beans you want to add:"
+            State.FILL_CUPS -> "Write how many disposable cups you want to add:"
+        }
     }
 }
--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,5 +1,10 @@
 package machine
 
 fun main() {
-    CoffeeMachine().run()
+    val coffeeMachine = CoffeeMachine()
+    do {
+        println(coffeeMachine)
+        val input = readln().trim().lowercase()
+        coffeeMachine.processInput(input)
+    } while (input != "exit")
 }
